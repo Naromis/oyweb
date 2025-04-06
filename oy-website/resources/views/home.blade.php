50ad5d7c3e35b0a199a84043f700096cd1581bf9
Fynn <fynn@wassmann>
2025-04-06 17:02:16 +0200

    first commit

---
+++ oy-website/resources/views/home.blade.php
@@ -0,0 +1,49 @@
+<html>
+<div style="width: 100%; height: 100%; position: relative; background: #F3F3F2; overflow: hidden">
+    <img style="width: 900px; height: 506px; left: 50px; top: 180px; position: absolute; border-radius: 25px" src="https://placehold.co/900x506" />
+    <div style="width: 10.96px; height: 14.54px; left: 339px; top: 638.01px; position: absolute; background: white"></div>
+    <div style="width: 15.11px; height: 14.55px; left: 359.88px; top: 638px; position: absolute; background: white"></div>
+    <div style="width: 11.47px; height: 14.23px; left: 386.24px; top: 638.16px; position: absolute; background: white"></div>
+    <div style="width: 12.22px; height: 14.22px; left: 409.03px; top: 638.16px; position: absolute; background: white"></div>
+    <div style="width: 2.03px; height: 14.22px; left: 433.65px; top: 638.16px; position: absolute; background: white"></div>
+    <div style="width: 14.90px; height: 14.23px; left: 445.92px; top: 638.16px; position: absolute; background: white"></div>
+    <div style="width: 21.57px; height: 14.22px; left: 483.18px; top: 638.16px; position: absolute; background: white"></div>
+    <div style="width: 12.02px; height: 14.39px; left: 515.55px; top: 638.16px; position: absolute; background: white"></div>
+    <div style="width: 9.73px; height: 14.22px; left: 539.86px; top: 638.16px; position: absolute; background: white"></div>
+    <div style="width: 9.73px; height: 14.22px; left: 560.05px; top: 638.16px; position: absolute; background: white"></div>
+    <div style="width: 12.45px; height: 14.23px; left: 580.25px; top: 638.16px; position: absolute; background: white"></div>
+    <div style="width: 15.11px; height: 14.55px; left: 601.82px; top: 638px; position: absolute; background: white"></div>
+    <div style="width: 11.47px; height: 14.23px; left: 628.17px; top: 638.16px; position: absolute; background: white"></div>
+    <div style="width: 10.04px; height: 14.22px; left: 650.96px; top: 638.16px; position: absolute; background: white"></div>
+    <div style="width: 900px; height: 506px; left: 50px; top: 1232px; position: absolute; background: #696969; border-radius: 25px"></div>
+    <img style="width: 900px; height: 506px; left: 970px; top: 180px; position: absolute; border-radius: 25px" src="https://placehold.co/900x506" />
+    <div style="width: 900px; height: 506px; left: 50px; top: 1758px; position: absolute; background: #696969; border-radius: 25px"></div>
+    <div style="width: 900px; height: 506px; left: 50px; top: 706px; position: absolute; background: #696969; border-radius: 25px"></div>
+    <div style="width: 900px; height: 506px; left: 970px; top: 1232px; position: absolute; background: #696969; border-radius: 25px"></div>
+    <div style="width: 900px; height: 506px; left: 970px; top: 706px; position: absolute; background: #696969; border-radius: 25px"></div>
+    <div style="width: 900px; height: 506px; left: 970px; top: 1758px; position: absolute; background: #696969; border-radius: 25px"></div>
+    <div style="width: 1920px; height: 180px; left: 0px; top: 0px; position: absolute">
+        <div style="width: 1920px; height: 180px; left: 0px; top: 0px; position: absolute"></div>
+        <div style="width: 25px; height: 10.88px; left: 1844px; top: 64.57px; position: absolute; outline: 3px #333333 solid; outline-offset: -1.50px"></div>
+        <div style="width: 25px; height: 37px; left: 51px; top: 54px; position: absolute">
+            <div style="width: 25px; height: 37px; left: 0px; top: 0px; position: absolute; background: #333333"></div>
+        </div>
+    </div>
+    <div style="width: 1920px; height: 250px; left: 0px; top: 2444px; position: absolute">
+        <div style="width: 162px; height: 43px; left: 689px; top: 82.89px; position: absolute; text-align: center; color: #333333; font-size: 30px; font-family: Montserrat; font-weight: 500; word-wrap: break-word">Services</div>
+        <div style="width: 96px; height: 43px; left: 518px; top: 82.89px; position: absolute; text-align: center; color: #333333; font-size: 30px; font-family: Montserrat; font-weight: 500; word-wrap: break-word">Home</div>
+        <div style="width: 93px; height: 43px; left: 926px; top: 82.89px; position: absolute; text-align: center; color: #333333; font-size: 30px; font-family: Montserrat; font-weight: 500; word-wrap: break-word">Cases</div>
+        <div style="width: 106px; height: 43px; left: 1094px; top: 82.89px; position: absolute; text-align: center; color: #333333; font-size: 30px; font-family: Montserrat; font-weight: 500; word-wrap: break-word">About</div>
+        <div style="width: 126px; height: 43px; left: 1275px; top: 82.89px; position: absolute; text-align: center; color: #333333; font-size: 30px; font-family: Montserrat; font-weight: 500; word-wrap: break-word">Kontakt</div>
+        <div style="width: 98px; height: 18px; left: 1591px; top: 192.89px; position: absolute; text-align: right; color: #333333; font-size: 15px; font-family: Montserrat; font-weight: 500; word-wrap: break-word">Impressum</div>
+        <div style="width: 177px; height: 18px; left: 1699px; top: 192.89px; position: absolute; text-align: right; color: #333333; font-size: 15px; font-family: Montserrat; font-weight: 500; word-wrap: break-word">Datenschutzerklärung</div>
+        <div style="width: 53.30px; height: 53.30px; left: 50px; top: 175px; position: absolute; background: white; border-radius: 9999px"></div>
+        <div style="width: 26px; height: 26px; left: 64px; top: 188px; position: absolute; background: #1E3AD9"></div>
+        <div style="width: 53.30px; height: 53.30px; left: 128px; top: 175px; position: absolute; background: white; border-radius: 9999px"></div>
+        <div style="width: 26px; height: 17px; left: 142px; top: 193px; position: absolute; background: #1E3AD9"></div>
+        <div style="width: 53.30px; height: 53.30px; left: 206px; top: 175px; position: absolute; background: white; border-radius: 9999px"></div>
+        <div style="width: 23px; height: 15px; left: 223px; top: 196px; position: absolute; background: #1E3AD9"></div>
+        <div style="width: 5px; height: 5px; left: 223px; top: 189px; position: absolute; background: #1E3AD9"></div>
+    </div>
+</div>
+</html>
